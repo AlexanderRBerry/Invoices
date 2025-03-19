948ce0b1fcf9ab07c64f427f3d78980649c2da5e
Alex Berry <Alexanderberry@mail.weber.edu>
2025-03-19 09:10:31 -0600

    Add project files.

---
+++ Main/clsMainSQL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoices.Main
+{
+    public class clsMainSQL
+    {
+    }
+}
