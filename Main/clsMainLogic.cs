a960b76fe4e7d39a482ae6c929377ad67f4b12a6
tylerbirch14 <tylerbirch14@gmail.com>
2025-04-16 17:19:50 -0600

    Updated wndMain, clsMainLogic, and clsMainSQL

--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -1,16 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Invoices.Common;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 
 namespace Invoices.Main
 {
     public class clsMainLogic
     {
-        //GetAllItems returns List<clsItem>
+        public List<clsItem> GetAllItems()
+        {
+            try
+            {
+                List<clsItem> items = new List<clsItem>();
+                // Connection to the database
+                clsDataAccess db = new clsDataAccess();
+                // Class holding relevant SQL statements
+                clsMainSQL sqlStatements = new clsMainSQL();
+                // SQL to retreive all items
+                string sSQL = sqlStatements.GetItemsSQL();
+                int rows = 0;
+                // Dataset holding all items
+                DataSet ds = db.ExecuteSQLStatement(sSQL, ref rows);
+
+                // Add all items to items list
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    // object to hold item details
+                    clsItem item = new clsItem();
+
+                    // Fill each item attribute with data from the database
+                    item.itemCode = row[0].ToString();
+                    item.itemDescription = row[1].ToString();
+                    item.cost = row[2].ToString();
+
+                    // Add the completed item to items list
+                    items.Add(item);
+                }
+                return items;
+            }
+            catch (Exception ex)
+            {
+                //Throw an exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+
         //SaveNewInvoice(clsInvoice)
+        public static void SaveNewInvoice(string date)
+        {
+            clsDataAccess db = new clsDataAccess();
+            clsMainSQL sqlStatements = new clsMainSQL();
+            string sSQL = sqlStatements.InsertInvoiceSQL(date, 0);//cost initially is $0, is updated as items are added
+            db.ExecuteNonQuery(sSQL);
+        }
+        public static void AddItem(int invoiceNum, int lineItemNum, string itemCode)
+        {
+            clsDataAccess db = new clsDataAccess();
+            clsMainSQL sqlStatements = new clsMainSQL();
+            string sSQL = sqlStatements.InsertItemsSQL(invoiceNum, lineItemNum, itemCode);
+            db.ExecuteNonQuery(sSQL);
+        }
+        //gets InvoiceNum given a certain date
+        public static int GetInvoiceNumber(string date)
+        {
+            clsDataAccess db = new clsDataAccess();
+            clsMainSQL sqlStatements = new clsMainSQL();
+            string sSQL = sqlStatements.GetInvoiceNumSQL(date);
+            int number = db.ExecuteNonQuery(sSQL);
+            return number;
+        }
         //EditInvoice(clsOldInvoice, clsNewInvoice)
+        public static void EditInvoice(int cost, int invoiceNum)
+        {
+            clsDataAccess db = new clsDataAccess();
+            clsMainSQL sqlStatements = new clsMainSQL();
+            string sSQL = sqlStatements.UpdateInvoicesSQL(cost, invoiceNum);
+            db.ExecuteNonQuery(sSQL);
+        }
         //GetInvoice(InvoiceNumber) returns clsInvoice - Get the invoice and items for the selected invoice from search window
+        public List<clsItem> GetInvoice(int invoiceNum)
+        { 
+            List<clsItem> invoices = new List<clsItem>();
+            clsDataAccess db = new clsDataAccess();
+            // Class holding relevant SQL statements
+            clsMainSQL sqlStatements = new clsMainSQL();
+            // SQL to retreive all items
+            string sSQL = sqlStatements.GetLineItemsSQL(invoiceNum);
+            int rows = 0;
+            // Dataset holding all items
+            DataSet ds = db.ExecuteSQLStatement(sSQL, ref rows);
+            // Add all items to items list
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                // object to hold item details
+                clsItem item = new clsItem();
+
+                // Fill each item attribute with data from the database
+                item.itemCode = row[0].ToString();
+                item.itemDescription = row[1].ToString();
+                item.cost = row[2].ToString();
+
+                // Add the completed item to items list
+                invoices.Add(item);
+            }
+
+            return invoices;
+        }
     }
 }
--- Main/wndMain.xaml.cs
+++ Main/wndMain.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Invoices.Common;
+using System.Reflection;
+using Invoices.Items;
 
 namespace Invoices.Main
 {
@@ -20,21 +23,114 @@ namespace Invoices.Main
     /// </summary>
     public partial class wndMain : Window
     {
+        wndSearch search;
+        wndItems items;
         public wndMain()
         {
             InitializeComponent();
             Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose; // Ensures complete shutdown
+            clsMainLogic mainLogic = new clsMainLogic();
+            List<clsItem> itemDetails = mainLogic.GetAllItems();
+            
+            for (int i = 0; i < itemDetails.Count; i++)
+            {
+                cbItems.Items.Add(itemDetails[i].itemDescription);
+            }
         }
+
         //after search window is closed, check property SelectedInvoiceID in the search window to see if an invoice is selected. If so load the invoice
 
         //after items window is closed, check property HasItemsBeenChanged in the Items window to see if any items were updated. If so re-load items in combo box.
 
         private void btnEditInvoice_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void btnSaveInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCreateInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            int invoiceNum = 0;
+            if (InvoiceDate.Text == null)
+            { return; }
+            string date = InvoiceDate.Text;
+            clsMainLogic.SaveNewInvoice(date);
+            invoiceNum = clsMainLogic.GetInvoiceNumber(date);
+            PopulateNewInvoice(invoiceNum);
+        }
+
+        private void cbItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selected = cbItems.SelectedItem as string;
+            clsMainLogic mainLogic = new clsMainLogic();
+            List<clsItem> itemDetails = mainLogic.GetAllItems();
+            for (int i = 0; i < itemDetails.Count; i++)
+            {
+                if (selected == itemDetails[i].itemDescription)
+                { Cost.Content = "$" + itemDetails[i].cost; }
+            }
+        }
+
+        //Populating certain invoice
+        private void PopulateInvoice()
+        {
+            clsMainLogic mainLogic = new clsMainLogic();
+            wndSearch invoiceNum = new wndSearch();
+            List<clsItem> invoice = mainLogic.GetInvoice(invoiceNum.selectedInvoiceID);
+            dgInvoice.ItemsSource = invoice;
+        }
+        //populate invoice given invoicenum
+        private void PopulateNewInvoice(int invoiceNum)
+        {
+            clsMainLogic mainLogic = new clsMainLogic();
+            List<clsItem> invoice = mainLogic.GetInvoice(invoiceNum);
+            dgInvoice.ItemsSource = invoice;
+        }
+
+        //Search Window
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                search = new wndSearch();
+                search.ShowDialog();
+                int invoice = 0;
+                invoice = wndSearch.selectedInvoiceID;
+                PopulateInvoice();
+            }
+            catch (Exception ex)
+            {
+                //Throw an exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                items = new wndItems();
+                items.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                //Throw an exception
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." +
+                                    MethodInfo.GetCurrentMethod().Name + " -> " + ex.Message);
+            }
+        }
+
+        private void btnAddItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
         {
 
         }
